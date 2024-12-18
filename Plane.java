860f8fef35587be848af02e0bb46f215169c3f2f
hoshikuzuryuu <yaminoiroha@gmail.com>
2024-12-18 10:17:48 -0800

    gagafafafadadad

--- Plane.java
+++ Plane.java
@@ -1,4 +1,33 @@
 public class Plane
 {
-  // implement the Plane class here
+  private int location;
+
+  public Plane()
+  {
+    this.location = 0;
+  }
+  public Plane(int loc)
+  {
+    if (loc >= -2000 && loc<= 2000)
+    {
+      this.location = loc;
+    }
+    else
+    {
+      this.location = 0;
+    }
+  }
+  public void upward()
+  {
+    if (this.location <= 2000)
+    {
+      this.location += 100;
+    }
+    else
+    {
+      System.out.println("You may not exceed 2000 feet!");
+    }
+  }
+
+  
 }
--- Student.java
+++ Student.java
@@ -11,6 +11,44 @@ public class Student
     grade = input_grade;
     name = input_name;
   }
+  public Student(String name, int grade, int id)
+  {
+    this.name = name;
+    this.grade = grade;
+    this.ID = id;
+  }
+  public Student(int grade, String name, int id)
+  {
+    this.grade = grade;
+    this.name = name;
+    this.ID = id; 
+  }
+
+  public void setName(String name)
+  {
+    this.name = name;
+  }
+  public void setGrade(int grade)
+  {
+    this.grade = grade; 
+  }
+  public void setID(int id)
+  {
+    this.ID = id;
+  }
+
+  public String getName()
+  {
+    return this.name;
+  }
+  public int getID()
+  {
+    return this.ID;
+  }
+  public int getGrade()
+  {
+    return this.grade;
+  }
 
   public String toString()
   {
